e9d26ade3a273f373e7d5fc8aac0434e5c119e22
Caleb Hsu <chialo.hsu@colorado.edu>
2016-09-29 13:27:45 -0600

    Hacky way of getting tag data in, Firebase ref not working

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../index.scss';
-
 import React from 'react';
 import BarChartViz from './barchart.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../index.scss';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <BarChartViz/>
+        <BarChartViz data={this.props.data} />
       </div>
     )
   }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
 import { render } from 'react-dom';
+import * as firebase from 'firebase';
 import App from './app.jsx';
 
-render(<App/>, document.querySelector("#app"));
+const config = {
+  apiKey: "AIzaSyAu5HYmep8Ik_MDeJEcd5AMwDVqknn8E84",
+  authDomain: "craftml-io-development.firebaseapp.com",
+  databaseURL: "https://craftml-io-development.firebaseio.com",
+  storageBucket: "craftml-io-development.appspot.com",
+  messagingSenderId: "728797607641"
+};
+
+firebase.initializeApp(config);
+
+var craftDb = firebase.database();
+var modelDbRef = craftDb.ref( 'docs' );
+
+var modelCollection, modelSample;
+var modelSampleSize = 100;
+
+var regexCube     = /<cube/gi;
+var regexCylinder = /<cylinder/gi;
+var regexSphere   = /<sphere/gi;
+
+var tagCounts =
+[{
+    'cube':     0,
+    'cylinder': 0,
+    'sphere':   0
+}]
+
+modelDbRef.once( 'value', function( snapshot ){
+  // creates array of model code, excluding empty code
+  modelCollection = _.pull(
+                    _.map( snapshot.val(), function( prop, modelId ){
+                          if( prop[ 'content' ] ){
+                              return prop[ 'content' ];
+                          }
+                    } ) , undefined );
+
+  // gets random sample of 100 models in array
+  modelSample = _.sampleSize( modelCollection, modelSampleSize );
+
+  // returns collection of tags & tag counts
+  _.forEach( modelSample , function( content, index ){
+      tagCounts[ 'cube' ]     += ( content.match( regexCube ) || [] ).length
+      tagCounts[ 'cylinder' ] += ( content.match( regexCylinder ) || [] ).length
+      tagCounts[ 'sphere' ]   += ( content.match( regexSphere ) || [] ).length
+  } );
+} );
+
+
+render(<App data={tagCounts} />, document.querySelector("#app"));
